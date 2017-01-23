8cec3725af1731349d76cec5c0ddcf79bc7f8e08
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-01-23 09:47:23 -0600

    Added lab1_part4.py instructions to repo

---
+++ lab1_part4.py
@@ -0,0 +1,12 @@
+# Write a program that fetches data from a web server using an open API.
+#  Ask me for the server you should use and the specific data you should fetch.
+
+# You'll need to do some research into how to carry out these tasks in Python. (Hint: the requests library)
+
+# Your Python program will make a request to the website, and get data as JSON.
+#  You'll need to process this to extract the data you need.
+
+# You should handle errors, such as access denied, no network connection, invalid response....
+
+# Extra challenge: your program should cache data locally to avoid excessive requests.
+#  If the user has made a request recently, your program should fetch the data from the cache instead of the website.
