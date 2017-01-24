66bb0e19c771f71227f724bada5ef2a2787626a5
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2017-01-24 04:22:10 -0600

    added 3 option menu, request try-catch"

--- lab1_part4.py
+++ lab1_part4.py
@@ -9,4 +9,72 @@
 # You should handle errors, such as access denied, no network connection, invalid response....
 
 # Extra challenge: your program should cache data locally to avoid excessive requests.
-#  If the user has made a request recently, your program should fetch the data from the cache instead of the website.
+# If the user has made a request recently, your program should fetch the data from the cache instead of the website.
+import requests
+
+
+# basic request code from borrowed http://docs.python-requests.org/en/master/user/quickstart/
+def get_json_data():
+
+    try:
+
+        # if request is successful - return request object, timeout in milliseconds
+        r = requests.get("http://svc.metrotransit.org/", timeout=500)
+
+        return r
+
+    except requests.exceptions.Timeout as to:
+
+        print("Your request timed-out.", to)
+
+    except requests.ConnectionError as ce:
+
+        print("There was a problem with your connection", ce)
+
+    # catch other request errors
+    except requests.RequestException as ex:
+
+        print("An error occurred: ", ex)
+
+
+def get_all_routes():
+    pass
+
+
+def get_select_route():
+    pass
+
+
+# used to display menu
+def menu():
+
+    selection = input("""Select an option from the menu:
+    1. view all routes
+    2. select route
+    3. exit
+    \n """)
+
+    return selection
+
+if __name__ == "__main__":
+
+    selection = menu()
+
+    # if selection is not one of our options, keep trying
+    while selection not in ['1', '2', '3']:
+        selection = menu()
+
+    if selection == '1':
+
+        get_all_routes()
+
+    elif selection == '2':
+
+        get_select_route()
+
+    elif selection == '3':
+        print('goodbye')
+        exit()
+    else:
+        print("default exit")
+        exit()
